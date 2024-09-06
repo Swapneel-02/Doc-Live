3458e988b2380e72bd085be9e27945d372aa7138
Swapneel Das <swapneel155@gmail.com>
2024-09-06 11:49:34 +0530

    Solved issue with clerk

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,11 @@
 import { SignIn } from '@clerk/nextjs'
+import React from 'react'
 
 const SignInPage = () => {
   return (
     <main className="auth-page">
-      <SignIn/>
+        <SignIn/>
+
     </main>
   )
 }
